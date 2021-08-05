ecc33ebfd8817a5bd0a039e042b897d9cb7f8c82
Dalerjon <dalerjon.azimov@gmail.com>
2021-08-05 15:32:35 +0200

    second commit

--- src/package1/ClassOne.java
+++ src/package1/ClassOne.java
@@ -3,6 +3,8 @@ package package1;
 public class ClassOne {
     public static void main(String[] args) {  //psvm or main is short cut of main method
         System.out.println("Hello World");
+        System.out.println("Batch 35/36 Learn GitHub");
+        System.out.println("Dalerjon is future AI master");
     }
 
 
