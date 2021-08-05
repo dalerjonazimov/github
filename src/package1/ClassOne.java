eb1ddc9c01706fad69527ac5357d2ba7aac46a75
Dalerjon <dalerjon.azimov@gmail.com>
2021-08-05 15:18:19 +0200

    first initial commit

---
+++ src/package1/ClassOne.java
@@ -0,0 +1,13 @@
+package package1;
+
+public class ClassOne {
+    public static void main(String[] args) {  //psvm or main is short cut of main method
+        System.out.println("Hello World");
+    }
+
+
+
+
+
+
+}
